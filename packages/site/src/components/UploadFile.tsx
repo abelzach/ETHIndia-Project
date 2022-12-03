c92e527461fd8938a67a5f8dbb8afb9330066aef
Alan Philipose <alanphilipose@gmail.com>
2022-12-03 09:18:09 +0530

    Initial commit

---
+++ packages/site/src/components/UploadFile.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react';
+import styled from 'styled-components';
+
+const UploadFile = styled.input.attrs(() => ({
+  type: 'file',
+}))`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding: 1rem;
+  ${({ theme }) => theme.mediaQueries.small} {
+    width: 100%;
+  }
+`;
+
+export const UploadFileInput = (props: ComponentProps<typeof UploadFile>) => {
+  return <UploadFile {...props} />;
+};
---
+++ packages/snap/src/index.ts
@@ -0,0 +1,52 @@
+import { OnRpcRequestHandler } from '@metamask/snap-types';
+
+/**
+ * Get a message from the origin. For demonstration purposes only.
+ *
+ * @param originString - The origin string.
+ * @returns A message based on the origin.
+ */
+export const getMessage = (originString: string): string =>
+  `Hello, ${originString}!`;
+
+/**
+ * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ *
+ * @param args - The request handler args as object.
+ * @param args.origin - The origin of the request, e.g., the website that
+ * invoked the snap.
+ * @param args.request - A validated JSON-RPC request object.
+ * @returns `null` if the request succeeded.
+ * @throws If the request method is not valid for this snap.
+ * @throws If the `snap_confirm` call failed.
+ */
+export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+  switch (request.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: getMessage(origin),
+            description:
+              'This custom confirmation is just for display purposes.',
+            textAreaContent:
+              'But you can edit the snap source code to make it do something, if you want to!',
+          },
+        ],
+      });
+    case 'upload':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: getMessage(origin),
+            description: 'Inside Snap/src/index.ts',
+            textAreaContent: 'You Uploaded a file ',
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
